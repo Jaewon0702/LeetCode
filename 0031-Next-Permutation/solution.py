f712fecc34ee9acd1e0f0ef4ec399421e94172ef
Jaewon(Noah) Lee <67958329+Jaewon0702@users.noreply.github.com>
2025-06-27 16:57:26 +0900

    Review 1

--- 0031-Next-Permutation/solution.py
+++ 0031-Next-Permutation/solution.py
@@ -7,19 +7,19 @@ class Solution:
         i = len(nums) - 2
         while i >= 0 and nums[i] >= nums[i+1]:
             i -= 1
-        
+
         # Step 2: Find the next larger number to swap with nums[i]
+        j = len(nums) - 1
         if i >= 0:
-            j = len(nums) - 1
-            while nums[j] <= nums[i]:
+            while nums[i] >= nums[j]:
                 j -= 1
 
         # Step 3: Swap
-            nums[i], nums[j] = nums[j], nums[i]
-            
-         # Step 4: Reverse the subarray from i+1 to end
+        nums[i], nums[j] = nums[j], nums[i]
+        
+        # Step 4: Reverse the subarray from i+1 to end
         left, right = i+1, len(nums) - 1
         while left < right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
-            right -= 1 
+            right -= 1
