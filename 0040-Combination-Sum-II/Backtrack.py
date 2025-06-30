78eed448877e66c4a045e92776e9e8d67041c72f
Jaewon(Noah) Lee <67958329+Jaewon0702@users.noreply.github.com>
2025-06-30 17:31:54 +0900

    Create Backtrack.py

---
+++ 0040-Combination-Sum-II/Backtrack.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        current = []
+        candidates.sort()
+        def DFS(start: int, total: int):
+            if current in result: ## To avoid duplicate combinations
+                return
+            if total == target:
+                result.append(current[:])
+                return
+            elif total < target:
+                for j in range(start, len(candidates)):
+                    if j > start and candidates[j] == candidates[j - 1]:
+                        continue
+                    current.append(candidates[j])
+                    DFS(j+1, total + candidates[j])
+                    current.pop()
+        DFS(0,0)
+        return result
