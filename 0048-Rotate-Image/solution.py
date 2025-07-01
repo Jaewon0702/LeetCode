00fb5544412309a32d7bec6cf8c34b8c8e860174
Jaewon(Noah) Lee <67958329+Jaewon0702@users.noreply.github.com>
2025-07-01 22:10:42 +0900

    Update solution.py

--- 0048-Rotate-Image/solution.py
+++ 0048-Rotate-Image/solution.py
@@ -4,8 +4,10 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
+        # Transpose
         for i in range(n):
             for j in range(i+1,n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # Reverse row
         for row in matrix:
             row.reverse()
