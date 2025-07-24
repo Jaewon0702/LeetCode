2db275a3f9180d4935d3606cf96ded2c01291118
Jaewon(Noah) Lee <67958329+Jaewon0702@users.noreply.github.com>
2025-07-24 16:47:20 +0900

    Create solution_stack.py

---
+++ 0071-Simplify-Path/solution_stack.py
@@ -0,0 +1,10 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        parts = path.split('/')
+        stack = []
+        for s in parts:
+            if s == "" or s == '.': continue
+            elif s == '..':
+                if stack: stack.pop()
+            else: stack.append(s)
+        return '/' + '/'.join(stack)
